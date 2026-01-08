32f5083713c036a7021aa44af3551202576e1bba
Yuri Santiago <yurimateussantiago@gmail.com>
2026-01-08 18:42:46 -0300

    finish project

--- tests/cubevalidation.py
+++
@@ -1,5 +0,0 @@
-def cubevalidation(vertices, edges):
-    assert len(vertices)== 8
-    assert len(edges) == 12
-    print("Cube vertex count correct!")
-    #TO-DO: as edges tem que ter todas os mesmos comprimentos 
